a0e86aa89b8c1a00eef06b5d4c27d5fd5adde9b0
IrynaChebotar <iryna.chebotar2@gmail.com>
2023-09-15 12:45:53 +0200

    feedback jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,4 +10,51 @@ export class App extends Component {
     neutral: 0,
     bad: 0,
   };
+  handleFeedbackClick = type => {
+    this.setState(prevState => ({
+      ...prevState,
+      [type]: prevState[type] + 1,
+    }));
+  };
+
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    const totalFeedback = this.countTotalFeedback();
+    const { good } = this.state;
+    return totalFeedback === 0 ? 0 : Math.round((good / totalFeedback) * 100);
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    const totalFeedback = this.countTotalFeedback();
+    const positivePercentage = this.countPositiveFeedbackPercentage();
+    return (
+      <div>
+        <Section title="Please leave feedback">
+          <Feedback
+            options={['good', 'neutral', 'bad']}
+            onLeaveFeedback={this.handleFeedbackClick}
+          />
+        </Section>
+
+        <Section title="Statistics">
+          {totalFeedback ? (
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={totalFeedback}
+              positivePercentage={positivePercentage}
+            />
+          ) : (
+            <Notification />
+          )}
+        </Section>
+      </div>
+    );
+  }
 }
